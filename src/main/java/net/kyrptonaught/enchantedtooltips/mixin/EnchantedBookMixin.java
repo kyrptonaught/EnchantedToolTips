7982476bba2888b4c1298cea7f68715f16b5a239
kyrptonaught <anthony.butillo10@gmail.com>
2019-12-03 12:14:38 -0500

    Change Inject to Redirect for added compatibility

--- src/main/java/net/kyrptonaught/enchantedtooltips/mixin/EnchantedBookMixin.java
+++ src/main/java/net/kyrptonaught/enchantedtooltips/mixin/EnchantedBookMixin.java
@@ -3,38 +3,23 @@ package net.kyrptonaught.enchantedtooltips.mixin;
 import net.kyrptonaught.enchantedtooltips.EnchantToolTipHelper;
 import net.kyrptonaught.enchantedtooltips.EnchantedToolTipMod;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.EnchantedBookItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.text.Text;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 
 @Mixin(EnchantedBookItem.class)
-public class EnchantedBookMixin extends Item {
-
-    public EnchantedBookMixin(Settings item$Settings_1) {
-        super(item$Settings_1);
-    }
-
-    @Inject(method = "appendTooltip", at = @At("HEAD"), cancellable = true)
-    public void ETTM$appendTooltip(ItemStack itemStack_1, World world_1, List<Text> list_1, TooltipContext tooltipContext_1, CallbackInfo cbi) {
-        super.appendTooltip(itemStack_1, world_1, list_1, tooltipContext_1);
-        if (MinecraftClient.getInstance().currentScreen == null) {
-            ItemStack.appendEnchantments(list_1, EnchantedBookItem.getEnchantmentTag(itemStack_1));
-            return;
-        }
-        if (EnchantedToolTipMod.getConfig().enableForBooks) {
-            EnchantToolTipHelper.appendToolTip(list_1, EnchantedBookItem.getEnchantmentTag(itemStack_1), true);
-            cbi.cancel();
-            return;
-        }
-
+public class EnchantedBookMixin {
+    @Redirect(method = "appendTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;appendEnchantments(Ljava/util/List;Lnet/minecraft/nbt/ListTag;)V"))
+    private void ETTM$appendTooltip(List<Text> tooltip, ListTag enchantments) {
+        if (MinecraftClient.getInstance().currentScreen == null)
+            ItemStack.appendEnchantments(tooltip, enchantments);
+        else if (EnchantedToolTipMod.getConfig().enableForBooks)
+            EnchantToolTipHelper.appendToolTip(tooltip, enchantments, true);
     }
 }
--- src/main/java/net/kyrptonaught/enchantedtooltips/mixin/ItemStackMixin.java
+++ src/main/java/net/kyrptonaught/enchantedtooltips/mixin/ItemStackMixin.java
@@ -19,11 +19,9 @@ public class ItemStackMixin {
     @Redirect(method = "getTooltip", at = @At(
             value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;appendEnchantments(Ljava/util/List;Lnet/minecraft/nbt/ListTag;)V"))
     private void ETTM$addEnchantInfo(List<Text> list, ListTag enchants) {
-        if (MinecraftClient.getInstance().currentScreen == null || enchants.size() == 0) {
+        if (MinecraftClient.getInstance().currentScreen == null || enchants.size() == 0)
             ItemStack.appendEnchantments(list, enchants);
-            return;
-        }
-        if (EnchantedToolTipMod.getConfig().enableForItems)
+        else if (EnchantedToolTipMod.getConfig().enableForItems)
             EnchantToolTipHelper.appendToolTip(list, enchants, true);
     }
 }
