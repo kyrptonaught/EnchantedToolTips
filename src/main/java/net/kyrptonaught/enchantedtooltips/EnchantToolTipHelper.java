9a6c53ab25a26e87f6a27f9dd1aeb184ebd3f493
kyrptonaught <anthonybuttillo10@gmail.com>
2022-06-07 01:49:56 -0400

    port to 1.19

--- src/main/java/net/kyrptonaught/enchantedtooltips/EnchantToolTipHelper.java
+++ src/main/java/net/kyrptonaught/enchantedtooltips/EnchantToolTipHelper.java
@@ -10,10 +10,8 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -47,7 +45,7 @@ public class EnchantToolTipHelper {
 
     private static void appendKeyHandler(List<Text> list) {
         String msg = I18n.translate("enchantedtooltip.presssneak").replaceAll("KEY", I18n.translate("enchantedtooltip.KEY"));
-        list.add(new LiteralText(msg));
+        list.add(Text.literal(msg));
     }
 
 
@@ -60,42 +58,42 @@ public class EnchantToolTipHelper {
             Enchantment enchant = Registry.ENCHANTMENT.get(enchantID);
             if (enchant == null) {
                 if (options.displayMissingEnchant) {
-                    list.add(new LiteralText(enchantTag.getString("id")).formatted(Formatting.GOLD));
-                    list.add(new TranslatableText("enchantedtooltip.enchant.removed").formatted(Formatting.WHITE));
-                    list.add(new TranslatableText("enchantedtooltip.enchant.removed2").formatted(Formatting.WHITE));
+                    list.add(Text.literal(enchantTag.getString("id")).formatted(Formatting.GOLD));
+                    list.add(Text.translatable("enchantedtooltip.enchant.removed").formatted(Formatting.WHITE));
+                    list.add(Text.translatable("enchantedtooltip.enchant.removed2").formatted(Formatting.WHITE));
                 }
                 continue;
             }
             //name
-            MutableText lvl = new TranslatableText("enchantment.level." + enchantTag.getInt("lvl"));
-            MutableText name = new TranslatableText(enchant.getTranslationKey()).append(" ");
+            MutableText lvl = Text.translatable("enchantment.level." + enchantTag.getInt("lvl"));
+            MutableText name = Text.translatable(enchant.getTranslationKey()).append(" ");
             MutableText title = name.formatted(enchant.isCursed() ? Formatting.RED : Formatting.DARK_GREEN);
 
             if (options.combineLvlMaxLvl) {
-                Text maxLvl = new TranslatableText("enchantment.level." + enchant.getMaxLevel());
-                lvl.append(new LiteralText("/")).append(maxLvl);
+                Text maxLvl = Text.translatable("enchantment.level." + enchant.getMaxLevel());
+                lvl.append(Text.literal("/")).append(maxLvl);
             }
             if (enchantTag.getInt("lvl") > 1 || !options.hideLvlI)
                 name.append(lvl);
             list.add(title);
             //desc
             if (options.displayDescription) {
-                list.add(new LiteralText(" ").append(getEnchantDesc("enchantment." + enchantTag.getString("id").replace(":", ".") + ".desc")).formatted(Formatting.WHITE));
+                list.add(Text.literal(" ").append(getEnchantDesc("enchantment." + enchantTag.getString("id").replace(":", ".") + ".desc")).formatted(Formatting.WHITE));
             }
             //Level
             if (options.displayMaxLvl && !options.combineLvlMaxLvl) {
-                Text maxLvl = new TranslatableText("enchantment.level." + enchant.getMaxLevel());
-                list.add(new TranslatableText("enchantedtooltip.enchant.maxLevel").append(maxLvl).formatted(Formatting.WHITE));
+                Text maxLvl = Text.translatable("enchantment.level." + enchant.getMaxLevel());
+                list.add(Text.translatable("enchantedtooltip.enchant.maxLevel").append(maxLvl).formatted(Formatting.WHITE));
             }
             //applies to
             if (options.displayAppliesTo && (!options.appliesToBookOnly || isBook)) {
                 if (enchant.type != null)
-                    list.add(new TranslatableText("enchantedtooltip.enchant.applicableTo").append(new TranslatableText("enchantedtooltip.enchant.type." + enchant.type.name())).formatted(Formatting.WHITE));
+                    list.add(Text.translatable("enchantedtooltip.enchant.applicableTo").append(Text.translatable("enchantedtooltip.enchant.type." + enchant.type.name())).formatted(Formatting.WHITE));
             }
             //from
             if (options.displayModFrom) {
                 String mod = getFromMod(enchantID.getNamespace());
-                list.add(new TranslatableText("enchantedtooltip.enchant.from").formatted(Formatting.WHITE).append(new LiteralText(mod).formatted(Formatting.BLUE)));
+                list.add(Text.translatable("enchantedtooltip.enchant.from").formatted(Formatting.WHITE).append(Text.literal(mod).formatted(Formatting.BLUE)));
             }
         }
     }
@@ -109,7 +107,7 @@ public class EnchantToolTipHelper {
 
     private static Text getEnchantDesc(String enchantId) {
         if (EnchantedToolTipMod.getCustomEnchantsNames().containsKey(enchantId) && !EnchantedToolTipMod.getCustomEnchantsNames().get(enchantId).equals(""))
-            return new LiteralText(EnchantedToolTipMod.getCustomEnchantsNames().get(enchantId));
-        return new TranslatableText(enchantId);
+            return Text.literal(EnchantedToolTipMod.getCustomEnchantsNames().get(enchantId));
+        return Text.translatable(enchantId);
     }
 }
--- src/main/java/net/kyrptonaught/enchantedtooltips/config/ModMenuIntegration.java
+++ src/main/java/net/kyrptonaught/enchantedtooltips/config/ModMenuIntegration.java
@@ -9,7 +9,7 @@ import net.kyrptonaught.kyrptconfig.config.screen.ConfigScreen;
 import net.kyrptonaught.kyrptconfig.config.screen.ConfigSection;
 import net.kyrptonaught.kyrptconfig.config.screen.items.BooleanItem;
 import net.kyrptonaught.kyrptconfig.config.screen.items.TextItem;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -24,37 +24,37 @@ public class ModMenuIntegration implements ModMenuApi {
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return (screen) -> {
             ConfigOptions options = EnchantedToolTipMod.getConfig();
-            ConfigScreen configScreen = new ConfigScreen(screen, new TranslatableText("Enchanted ToolTips Config"));
+            ConfigScreen configScreen = new ConfigScreen(screen, Text.translatable("key.enchantedtooltips.config.title"));
             configScreen.setSavingEvent(() -> EnchantedToolTipMod.config.save());
 
-            ConfigSection mainSection = new ConfigSection(configScreen, new TranslatableText("key.enchantedtooltips.config.category.main"));
+            ConfigSection mainSection = new ConfigSection(configScreen, Text.translatable("key.enchantedtooltips.config.category.main"));
 
-            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.booksenabled"), options.enableForBooks, true).setSaveConsumer(val -> options.enableForBooks = val));
-            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.itemsenabled"), options.enableForItems, true).setSaveConsumer(val -> options.enableForItems = val));
-            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.tableenabled"), options.enableForEnchantTable, true).setSaveConsumer(val -> options.enableForEnchantTable = val));
+            mainSection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.booksenabled"), options.enableForBooks, true).setSaveConsumer(val -> options.enableForBooks = val));
+            mainSection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.itemsenabled"), options.enableForItems, true).setSaveConsumer(val -> options.enableForItems = val));
+            mainSection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.tableenabled"), options.enableForEnchantTable, true).setSaveConsumer(val -> options.enableForEnchantTable = val));
 
-            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.alwaysshow"), options.alwaysShowEnchantInfo, false).setSaveConsumer(val -> options.alwaysShowEnchantInfo = val));
-            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.displaypress"), options.displayPressForInfo, true).setSaveConsumer(val -> options.displayPressForInfo = val));
-            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.displaymissing"), options.displayMissingEnchant, true).setSaveConsumer(val -> options.displayMissingEnchant = val));
-            mainSection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.sortenchants"), options.sortEnchantInfo, false).setSaveConsumer(val -> options.sortEnchantInfo = val));
+            mainSection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.alwaysshow"), options.alwaysShowEnchantInfo, false).setSaveConsumer(val -> options.alwaysShowEnchantInfo = val));
+            mainSection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.displaypress"), options.displayPressForInfo, true).setSaveConsumer(val -> options.displayPressForInfo = val));
+            mainSection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.displaymissing"), options.displayMissingEnchant, true).setSaveConsumer(val -> options.displayMissingEnchant = val));
+            mainSection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.sortenchants"), options.sortEnchantInfo, false).setSaveConsumer(val -> options.sortEnchantInfo = val));
 
-            ConfigSection displaySection = new ConfigSection(configScreen, new TranslatableText("key.enchantedtooltips.config.category.display"));
-            displaySection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.hidelvli"), options.hideLvlI, false).setSaveConsumer(val -> options.hideLvlI = val));
-            displaySection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.displayapplies"), options.displayAppliesTo, true).setSaveConsumer(val -> options.displayAppliesTo = val));
-            displaySection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.appliesbookonly"), options.appliesToBookOnly, false).setSaveConsumer(val -> options.appliesToBookOnly = val));
-            displaySection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.displaydesc"), options.displayDescription, true).setSaveConsumer(val -> options.displayDescription = val));
-            displaySection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.displaymaxlvl"), options.displayMaxLvl, true).setSaveConsumer(val -> options.displayMaxLvl = val));
-            displaySection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.comblvlmaxlvl"), options.combineLvlMaxLvl, false).setSaveConsumer(val -> options.combineLvlMaxLvl = val));
-            displaySection.addConfigItem(new BooleanItem(new TranslatableText("key.enchantedtooltips.config.displaymodfrom"), options.displayModFrom, true).setSaveConsumer(val -> options.displayModFrom = val));
+            ConfigSection displaySection = new ConfigSection(configScreen, Text.translatable("key.enchantedtooltips.config.category.display"));
+            displaySection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.hidelvli"), options.hideLvlI, false).setSaveConsumer(val -> options.hideLvlI = val));
+            displaySection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.displayapplies"), options.displayAppliesTo, true).setSaveConsumer(val -> options.displayAppliesTo = val));
+            displaySection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.appliesbookonly"), options.appliesToBookOnly, false).setSaveConsumer(val -> options.appliesToBookOnly = val));
+            displaySection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.displaydesc"), options.displayDescription, true).setSaveConsumer(val -> options.displayDescription = val));
+            displaySection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.displaymaxlvl"), options.displayMaxLvl, true).setSaveConsumer(val -> options.displayMaxLvl = val));
+            displaySection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.comblvlmaxlvl"), options.combineLvlMaxLvl, false).setSaveConsumer(val -> options.combineLvlMaxLvl = val));
+            displaySection.addConfigItem(new BooleanItem(Text.translatable("key.enchantedtooltips.config.displaymodfrom"), options.displayModFrom, true).setSaveConsumer(val -> options.displayModFrom = val));
 
-            ConfigSection enchantSection = new ConfigSection(configScreen, new TranslatableText("key.enchantedtooltips.config.category.enchants"));
+            ConfigSection enchantSection = new ConfigSection(configScreen, Text.translatable("key.enchantedtooltips.config.category.enchants"));
 
             List<Identifier> enchants = new ArrayList<>(Registry.ENCHANTMENT.getIds());
             Collections.sort(enchants);
             enchants.forEach(identifier -> {
                 String id = identifier.toString();
                 String enchant = "enchantment." + id.replace(":", ".") + ".desc";
-                enchantSection.addConfigItem(new TextItem(new TranslatableText(enchant.substring(12, enchant.length() - 5)), EnchantedToolTipMod.getCustomEnchantsNames().getOrDefault(enchant, ""), "").setSaveConsumer(val -> {
+                enchantSection.addConfigItem(new TextItem(Text.translatable(enchant.substring(12, enchant.length() - 5)), EnchantedToolTipMod.getCustomEnchantsNames().getOrDefault(enchant, ""), "").setSaveConsumer(val -> {
                     if (val.equals("")) EnchantedToolTipMod.getCustomEnchantsNames().remove(enchant);
                     else EnchantedToolTipMod.getCustomEnchantsNames().put(enchant, val);
                 }));
